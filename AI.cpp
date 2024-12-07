27ecd818e7ea2a4a158ac977c4a88261afa26e3a
sambar21 <sambatur@umich.edu>
2024-12-07 17:37:27 -0500

    Ai.cpp finalized

--- AI.cpp
+++ AI.cpp
@@ -4,8 +4,8 @@
  * AI.cpp
  * Project UID 848fee0125dbb5eb53ed294f20dbef81
  *
- * <#Names#>
- * <#Uniqnames#>
+ * <Varun Sambatur, John Butler, Daniel Joseph, Miguel Truschel>
+ * <>
  *
  * Final Project - Elevators
  */
@@ -17,67 +17,76 @@
 // You do not need to make any changes to this file for the Core
 
 string getAIMoveString(const BuildingState& buildingState) {
-    // Strategy overview:
-    // 1. If an elevator is idle and there's a floor with a request, service that floor
-    // 2. If no requests, pass
-    
-    for (int i = 0; i < NUM_ELEVATORS; ++i) {
-        // If this elevator is not servicing a request
-        if (!buildingState.elevators[i].isServicing) {
-            // Look for floors with up or down requests
-            for (int j = 0; j < NUM_FLOORS; ++j) {
-                if (buildingState.floors[j].hasUpRequest || 
-                    buildingState.floors[j].hasDownRequest) {
-                    
-                    // If elevator not already on this floor, service it
-                    if (buildingState.elevators[i].currentFloor != j) {
-                        return "e" + to_string(i) + "f" + to_string(j);
-                    }
-                    // If on this floor, do a pickup
-                    else {
-                        return "e" + to_string(i) + "p";
-                    }
+    int selectedElevator = -1; // tracks elevator chosen to service request
+    int targetRequestFloor = -1; // target floor
+    int shortestDistance = NUM_FLOORS + 1; // shortest distance to floor
+
+    for (int elevatorIndex = 0; elevatorIndex < NUM_ELEVATORS; ++elevatorIndex) {
+        // loop through each elevator skipping elevators servicing a request
+        if (buildingState.elevators[elevatorIndex].isServicing) {
+            continue;
+        }
+
+        // looping through all floors, based on whether its going up or down find the distance and go if closer request is found
+        for (int floorIndex = 0; floorIndex < NUM_FLOORS; ++floorIndex) {
+            if (buildingState.floors[floorIndex].hasUpRequest || buildingState.floors[floorIndex].hasDownRequest) {
+                int distanceToFloor = abs(buildingState.elevators[elevatorIndex].currentFloor - floorIndex);
+                if (distanceToFloor < shortestDistance) {
+                    selectedElevator = elevatorIndex;
+                    targetRequestFloor = floorIndex;
+                    shortestDistance = distanceToFloor;
                 }
             }
         }
     }
-    
-    // If no elevators can service a request, pass
-    return "";
+
+    // if selected elevator is found and target request floor is found
+    if (selectedElevator != -1 && targetRequestFloor != -1) {
+        if (buildingState.elevators[selectedElevator].currentFloor == targetRequestFloor) {
+            return "e" + to_string(selectedElevator) + "p"; // return "eX p" where X is elevator id
+        } else {
+            return "e" + to_string(selectedElevator) + "f" + to_string(targetRequestFloor); // return "eX fY" where X is elevator id and Y is target floor to travel to
+        }
+    }
+
+    return ""; // Pass if no requests are pending
 }
 
 
 string getAIPickupList(const Move& move, const BuildingState& buildingState, 
                        const Floor& floorToPickup) {
-    // Strategy: 
-    // 1. Pickup all people on the floor going in the same direction
-    // 2. Prioritize lower anger levels if possible
+    // Prioritize lower anger levels if possible while picking up people 
     
-    string pickupList = "";
-    bool isGoingUp = false;
+    string pickupIndicesList = "";
+    bool pickupDirectionUp = false;
     
-    // Determine direction of pickup
-    for (int i = 0; i < floorToPickup.getNumPeople(); ++i) {
-        Person person = floorToPickup.getPersonByIndex(i);
-        if (person.getTargetFloor() > person.getCurrentFloor()) {
-            isGoingUp = true;
+    // loop through all people on the floor and check if they are going up or down
+    for (int personIndex = 0; personIndex < floorToPickup.getNumPeople(); ++personIndex) {
+        Person currentPerson = floorToPickup.getPersonByIndex(personIndex);
+        if (currentPerson.getTargetFloor() > currentPerson.getCurrentFloor()) {
+            pickupDirectionUp = true;
             break;
         }
-        if (person.getTargetFloor() < person.getCurrentFloor()) {
-            isGoingUp = false;
+        if (currentPerson.getTargetFloor() < currentPerson.getCurrentFloor()) {
+            pickupDirectionUp = false;
             break;
         }
+        // if person target floor is equal to current floor, then they are on the same floor
+        // if person target floor is greater than current floor, then they are going up
+        // if person target floor is less than current floor, then they are going down
+        // pickupDirectionUp is a metric to demonstrate this 
     }
-    
-    // Select people going in the correct direction
-    for (int i = 0; i < floorToPickup.getNumPeople(); ++i) {
-        Person person = floorToPickup.getPersonByIndex(i);
-        bool personGoingUp = person.getTargetFloor() > person.getCurrentFloor();
+    // loop through again
+    // if direction matches pickupDirectionUp, then add person to pickup list
+    // making sure to append to the pickup list
+    for (int personIndex = 0; personIndex < floorToPickup.getNumPeople(); ++personIndex) {
+        Person currentPerson = floorToPickup.getPersonByIndex(personIndex);
+        bool currentPersonGoingUp = currentPerson.getTargetFloor() > currentPerson.getCurrentFloor();
         
-        if (personGoingUp == isGoingUp) {
-            pickupList += to_string(i);
+        if (currentPersonGoingUp == pickupDirectionUp) {
+            pickupIndicesList += to_string(personIndex);
         }
     }
     
-    return pickupList;
+    return pickupIndicesList;
 }
