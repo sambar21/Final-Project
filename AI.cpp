808e8c8af879901b18fbeea2f9a67d5e7dc91971
sambar21 <sambatur@umich.edu>
2024-12-11 10:56:59 -0500

    revised

--- AI.cpp
+++ AI.cpp
@@ -17,76 +17,90 @@
 // You do not need to make any changes to this file for the Core
 
 string getAIMoveString(const BuildingState& buildingState) {
-    int selectedElevator = -1; // tracks elevator chosen to service request
-    int targetRequestFloor = -1; // target floor
-    int shortestDistance = NUM_FLOORS + 1; // shortest distance to floor
-
-    for (int elevatorIndex = 0; elevatorIndex < NUM_ELEVATORS; ++elevatorIndex) {
-        // loop through each elevator skipping elevators servicing a request
-        if (buildingState.elevators[elevatorIndex].isServicing) {
-            continue;
-        }
-
-        // looping through all floors, based on whether its going up or down find the distance and go if closer request is found
-        for (int floorIndex = 0; floorIndex < NUM_FLOORS; ++floorIndex) {
-            if (buildingState.floors[floorIndex].hasUpRequest || buildingState.floors[floorIndex].hasDownRequest) {
-                int distanceToFloor = abs(buildingState.elevators[elevatorIndex].currentFloor - floorIndex);
-                if (distanceToFloor < shortestDistance) {
-                    selectedElevator = elevatorIndex;
-                    targetRequestFloor = floorIndex;
-                    shortestDistance = distanceToFloor;
+    int selectedElevator = -1;
+    int targetRequestFloor = -1;
+    int shortestDistance = NUM_FLOORS + 1;
+    
+    // check for angry passengers 
+    for (int floorIndex = 0; floorIndex < NUM_FLOORS; ++floorIndex) {
+        if (buildingState.floors[floorIndex].hasUpRequest || 
+            buildingState.floors[floorIndex].hasDownRequest) {
+            // check anger levels on  floor
+            for (int p = 0; p < buildingState.floors[floorIndex].numPeople; ++p) {
+                if (buildingState.floors[floorIndex].people[p].angerLevel >= 8) {
+                    // find closest available elevator
+                    for (int e = 0; e < NUM_ELEVATORS; ++e) {
+                        if (!buildingState.elevators[e].isServicing) {
+                            int distance = abs(buildingState.elevators[e].currentFloor - floorIndex);
+                            if (distance < shortestDistance) {
+                                selectedElevator = e;
+                                targetRequestFloor = floorIndex;
+                                shortestDistance = distance;
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 
-    // if selected elevator is found and target request floor is found
+    // If no urgent cases, handle normal requests
+    if (selectedElevator == -1) {
+        for (int e = 0; e < NUM_ELEVATORS; ++e) {
+            if (!buildingState.elevators[e].isServicing) {
+                for (int f = 0; f < NUM_FLOORS; ++f) {
+                    if (buildingState.floors[f].hasUpRequest || 
+                        buildingState.floors[f].hasDownRequest) {
+                        int distance = abs(buildingState.elevators[e].currentFloor - f);
+                        if (distance < shortestDistance) {
+                            selectedElevator = e;
+                            targetRequestFloor = f;
+                            shortestDistance = distance;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     if (selectedElevator != -1 && targetRequestFloor != -1) {
         if (buildingState.elevators[selectedElevator].currentFloor == targetRequestFloor) {
-            return "e" + to_string(selectedElevator) + "p"; // return "eX p" where X is elevator id
+            return "e" + to_string(selectedElevator) + "p";
         } else {
-            return "e" + to_string(selectedElevator) + "f" + to_string(targetRequestFloor); // return "eX fY" where X is elevator id and Y is target floor to travel to
+            return "e" + to_string(selectedElevator) + "f" + to_string(targetRequestFloor);
         }
     }
 
-    return ""; // Pass if no requests are pending
+    return "";
 }
 
 
-string getAIPickupList(const Move& move, const BuildingState& buildingState, 
-                       const Floor& floorToPickup) {
-    // Prioritize lower anger levels if possible while picking up people 
+
+string getAIPickupList(const Move& move, const BuildingState& buildingState,
+                      const Floor& floorToPickup) {
+    string pickupList = "";
+    int currentFloor = floorToPickup.getPersonByIndex(0).getCurrentFloor();
+    bool goingUp = false;
     
-    string pickupIndicesList = "";
-    bool pickupDirectionUp = false;
-    
-    // loop through all people on the floor and check if they are going up or down
-    for (int personIndex = 0; personIndex < floorToPickup.getNumPeople(); ++personIndex) {
-        Person currentPerson = floorToPickup.getPersonByIndex(personIndex);
-        if (currentPerson.getTargetFloor() > currentPerson.getCurrentFloor()) {
-            pickupDirectionUp = true;
-            break;
-        }
-        if (currentPerson.getTargetFloor() < currentPerson.getCurrentFloor()) {
-            pickupDirectionUp = false;
-            break;
-        }
-        // if person target floor is equal to current floor, then they are on the same floor
-        // if person target floor is greater than current floor, then they are going up
-        // if person target floor is less than current floor, then they are going down
-        // pickupDirectionUp is a metric to demonstrate this 
-    }
-    // loop through again
-    // if direction matches pickupDirectionUp, then add person to pickup list
-    // making sure to append to the pickup list
-    for (int personIndex = 0; personIndex < floorToPickup.getNumPeople(); ++personIndex) {
-        Person currentPerson = floorToPickup.getPersonByIndex(personIndex);
-        bool currentPersonGoingUp = currentPerson.getTargetFloor() > currentPerson.getCurrentFloor();
-        
-        if (currentPersonGoingUp == pickupDirectionUp) {
-            pickupIndicesList += to_string(personIndex);
+    //  determine direction based on angriest passenger
+    int maxAnger = -1;
+    for (int i = 0; i < floorToPickup.getNumPeople(); ++i) {
+        Person p = floorToPickup.getPersonByIndex(i);
+        if (p.getAngerLevel() > maxAnger) {
+            maxAnger = p.getAngerLevel();
+            goingUp = p.getTargetFloor() > currentFloor;
         }
     }
     
-    return pickupIndicesList;
+    // pick up passengers going in the same direction
+    for (int i = 0; i < floorToPickup.getNumPeople(); ++i) {
+        Person p = floorToPickup.getPersonByIndex(i);
+        bool personGoingUp = p.getTargetFloor() > currentFloor;
+        if (personGoingUp == goingUp) {
+            pickupList += to_string(i);
+        }
+    }
+    
+    return pickupList;
 }
+
